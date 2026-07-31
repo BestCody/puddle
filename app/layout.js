8e4ed4b0b4becb2adeabd43346c6ebbf25cfd7f8
Hansen <louhansen811@gmail.com>
2026-07-31 15:24:18 -0400

    Load real place photo styles

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import './auth.css'
 import './onboarding.css'
 import './date-swipe.css'
+import './real-place-photos.css'
 import './product.css'
 import './stage-two.css'
 import './stage-three-four.css'
