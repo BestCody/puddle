c36372bb4ae15430d7ac8a75f7d709a2d18a93ad
Hansen <louhansen811@gmail.com>
2026-08-15 23:36:55 -0400

    Support exact outline logo on dashboard

--- components/puddle-logo.js
+++ components/puddle-logo.js
@@ -1,9 +1,10 @@
 import Link from 'next/link'
 
-export function PuddleLogo({ compact = false, href = '/' }) {
+export function PuddleLogo({ compact = false, href = '/', variant = 'brand' }) {
+  const mark = variant === 'outline' ? '/puddle-mark-outline.svg' : '/puddle-mark.svg'
   return (
     <Link className="puddle-logo" href={href} aria-label={href === '/discover' ? 'Open Swipe' : 'Puddle home'}>
-      <img src="/puddle-mark.svg" alt="" width="44" height="44" />
+      <img src={mark} alt="" width="44" height="44" />
       {!compact && <span>puddle<span className="logo-dot">.you</span></span>}
     </Link>
   )
